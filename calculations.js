fc7e243a91076577670766048222559f368ec595
KrisGromova <levkindom@gmail.com>
2023-10-20 02:00:51 +0300

    fixed function_hw_2.2.1

--- calculations.js
+++ calculations.js
@@ -24,3 +24,4 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
+
